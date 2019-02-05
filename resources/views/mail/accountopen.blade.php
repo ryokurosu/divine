d31aa3bbba69d08bf75d9b063876065c9cea63f9
Ryo Kurosu <knowrop1208@gmail.com>
2019-02-05 18:31:48 +0900

    m

--- resources/views/mail/accountopen.blade.php
+++ resources/views/mail/accountopen.blade.php
@@ -9,9 +9,21 @@ life innovation systemでございます。
 
 口座開設ページ【zeus】
 https://gforex.asia/vip/74774/
+口座開設ページ【athena】
+https://gforex.asia/vip/75477/
 口座開設ページ【poseidon】
 https://gforex.asia/vip/75413/
 口座開設ページ【apollon】
+https://gforex.asia/vip/75414/
+
+
+口座開設ページ【Tiger】
+https://gforex.asia/vip/74774/
+口座開設ページ【Turtle】
+https://gforex.asia/vip/75477/
+口座開設ページ【Elephant】
+https://gforex.asia/vip/75413/
+口座開設ページ【Shark】
 https://gforex.asia/vip/75414/
 
 口座開設マニュアル
@@ -25,4 +37,5 @@ https://itunes.apple.com/jp/app/metatrader-4-%E9%80%9A%E8%B2%A8%E5%B8%82%E5%A0%B
 ②開設手続きが終わると登録いただいたメールアドレスに証券会社から「MT4ID」と「パスワード」が届きます。
 ③ご自分の口座に証拠金を入金してください。
 （自動取引のため、入金が反映された時点で運用が自動的に開始されます。）
-④「口座開設マニュアル」をご参照の上、「MT4アプリダウンロードページ」からご自身の運用の管理することができます。
+④「口座開設マニュアル」をご参照の上、「MT4アプリダウンロードページ」からご自身の運用の管理することができます
+
